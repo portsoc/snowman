983b56bb42d10ff7e3b88e9d174e5cf6f6a07e68
Rich Boakes <rjb@port.ac.uk>
2022-12-22 08:53:25 +0000

    Make  snowman animated by following the mouse.  uses --lean variable and sets mouse-x, mouse-y, mousevw and mouse-vh variables for future use.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+import * as mouse from "./lib/mouse.mjs";
 import * as snowman from "./lib/snowman.mjs";
 
 function init() {
-    snowman.init()
+    snowman.init();
+    mouse.init();
 }
 
 window.addEventListener('load',init);
